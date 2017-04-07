c2ff77a1cd30dcd32a83411eba1235b25b76bfa4
ThareqAliAkbar_6706154058 <ali.akbar2630@gmail.com>
2017-04-07 10:37:35 +0700

    Update Faktorial.java

--- Faktorial.java
+++ Faktorial.java
@@ -5,7 +5,7 @@ public class Faktorial {
 		int n = 12/2;
 		for(int i=1;i<=n;i++){
 			for(int k=n;k>i;k--){
-				System.out.print(" ");
+				System.out.print("0");
 			}
 			for(int j=1;j<=l;j++){
 				System.out.print("+");
@@ -18,7 +18,7 @@ public class Faktorial {
 		 
 		for(int i=(n-1);i>=1;i--){
 			for(int k=i;k<=(n-1);k++){
-				System.out.print(" ");
+				System.out.print("0");
 			}
 			for(int j=l;j>=1;j--){
 				System.out.print("+");
